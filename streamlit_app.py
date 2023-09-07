504c25f92d77f77d4b2ecb33e7cf1b24c2a466fa
Sergio Lucero <sergiolucero@gmail.com>
2023-09-07 10:40:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ st.set_page_config(layout="wide")
 
 #tab1, tab2 = st.tabs(['Allende','Rettig'])
 st.title('11+50: hable con Allende')
-st.image('Allende50.jfif')
+st.image('Allende50.jfif', width=400)
 st.write('creado como homenaje en Septiembre 2023 por Sergio Lucero')
