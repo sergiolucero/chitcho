272f3eae6850c50910fc5248d0bbc144c876e3dd
Sergio Lucero <sergiolucero@gmail.com>
2023-09-07 10:31:10 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,8 @@
 import streamlit as st
-from awslib import s3_contents
-from textlib import droplines
-from linelib import simple_recorder 
 ##############################################
 st.set_page_config(layout="wide")
 
-tab1, tab2 = st.tabs(['Grabación','Revisión'])
-with tab1:
-    st.title('👨‍⚕️CETRAM QuantMed LLM Doctor🤖')
-    for dropline in droplines:
-        st.write(dropline)
-    simple_recorder()
-with tab2:
-    st.header('Contents of cetram-felix/AUDIO')
-    contents = s3_contents()  
-    st.table(contents)
+#tab1, tab2 = st.tabs(['Allende','Rettig'])
+st.title('11+50: hable con Allende')
+st.image('Allende.jfif')
+st.write('creado como homenaje en Septiembre 2023 por Sergio Lucero')
