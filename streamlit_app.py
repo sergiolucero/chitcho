162efe7d79c81faafae9dd343870e0e26a6ee86e
Sergio Lucero <sergiolucero@gmail.com>
2023-09-12 19:08:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import os, streamlit as st
-
+import time
 from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, LLMPredictor, PromptHelper, ServiceContext
 from langchain.llms.openai import OpenAI
 
@@ -7,17 +7,17 @@ from langchain.llms.openai import OpenAI
 os.environ['OPENAI_API_KEY']= st.secrets['OPENAI_API_KEY']
 
 def get_response(query):
-    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="gpt-3.5-turbo-0613"))  # was text-davinci-003
 
     max_input_size = 8192;     num_output = 256
     max_chunk_overlap = 20;    dirpath = './docs'; idx_file = 'index.pkl'
-    prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
-    documents = SimpleDirectoryReader(dirpath).load_data()
-    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-    
+    t0 = time.time()
     if os.path.exists(idx_file):
         index = GPTSimpleVectorIndex.load_from_disk(idx_file)
     else:
+        prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
+        documents = SimpleDirectoryReader(dirpath).load_data()
+        service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+        llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="gpt-3.5-turbo-0613"))  # was text-davinci-003
         index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
         index.save_to_disk(idx_file)
             
@@ -25,7 +25,9 @@ def get_response(query):
     if response is None:
         st.error("Oops! No result found")
     else:
-        st.success(response)
+        dt = round(time.time()-t0,2)
+        tdt = f'[DT: {tdt} secs'
+        st.success(response + tdt)
 ##################################################################
 st.title("Chat con Chicho")
 st.image('Allende50.jfif', width=200)
