e907f1796e70f38388f30e0a0157bb776cd7f420
Sergio Lucero <sergiolucero@gmail.com>
2023-09-12 01:04:29 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,41 @@
-import streamlit as st
-##############################################
-st.set_page_config(page_title='Hable con Allende', layout='wide')
-st.markdown("<h3 style='text-align: center; color: blue;'>11+50: hable con Allende</h3>", unsafe_allow_html=True)
+import os, streamlit as st
 
-#tab1, tab2 = st.tabs(['Allende','Rettig'])
-col1, col2, col3 = st.columns([1,10,1])
+from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, LLMPredictor, PromptHelper, ServiceContext
+from langchain.llms.openai import OpenAI
 
-with col1:
-    st.write(' ')
-with col2:
-    st.image('Allende50.jfif', width=300)
-    st.write('creado como homenaje en Septiembre 2023 por Sergio Lucero')
-with col3:
-    st.write(' ')
-st.write('version 0.1')
+# Uncomment to specify your OpenAI API key here, or add corresponding environment variable (recommended)
+os.environ['OPENAI_API_KEY']= st.secrets['OPENAI_API_KEY']
+
+def get_response(query):
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
+
+    max_input_size = 4096
+    num_output = 256
+    max_chunk_overlap = 20
+    dirpath = './docs'
+    prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
+
+    if True: 
+        documents = SimpleDirectoryReader(dirpath).load_data()
+        service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+        index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+        
+        response = index.query(query)
+        if response is None:
+            st.error("Oops! No result found")
+        else:
+            st.success(response)
+
+# Define a simple Streamlit app
+st.title("Chat con Chicho")
+query = st.text_input("Pregúntele al Doctor Allende", "")
+
+# If the 'Submit' button is clicked
+if st.button("Pregunte"):
+    if not query.strip():
+        st.error(f"Please provide the search query.")
+    else:
+        try:
+            get_response(query)
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
